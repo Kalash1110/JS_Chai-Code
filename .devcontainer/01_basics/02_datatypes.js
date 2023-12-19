83695e90b7ea644cb92b40cf724d9339f3056a70
Kalash1110 <65525443+Kalash1110@users.noreply.github.com>
2023-12-19 06:22:31 +0000

    Conversions

--- .devcontainer/01_basics/02_datatypes.js
+++ .devcontainer/01_basics/02_datatypes.js
@@ -9,6 +9,8 @@ let loggedIn=false
  console.log(typeof null) // object type
  console.log(typeof undefined); //undefined type
 
+
+                //CONVERSIONS
  let score="33abc"
  //if score was undefined you get NaN in getNumber
  //if score was true you get 1 in getNumber
@@ -18,3 +20,43 @@ let loggedIn=false
  let getNumber=Number(score)
  console.log(typeof getNumber)
  console.log(getNumber)
+
+ let IsLoggedIn=1;
+let getBoolean=Boolean(IsLoggedIn)
+console.log(getBoolean);
+
+//If IsLoggedIn ="" ,gives false
+//If IsLoggedIn ="abc" ,gives true
+
+            //OPERATIONS
+
+let a=2**2 //Power
+console.log(a);
+
+console.log(2/3); //Not zero as output
+
+let str1="Kalash"
+let str2=" Jha"
+let str3=str1+str2 //String concatenation
+console.log(str3);
+
+console.log(1+2+"2"); //Number comes first hence addition then atlast string
+console.log("1"+2+2); //Starts from string,hence as string
+console.log(+true); //gives 1
+console.log(+""); //gives 0
+
+let num1,num2,num3
+num1=num2=num3=4  //All get values as 4
+console.log(num1);
+console.log(num2);
+console.log(num3);
+
+                //COMPARISONS
+
+console.log("2">1); //true
+console.log(null==0); //false null is not converted to number
+console.log(null>0); //false
+console.log(null>=0); //true,null is converted to number
+
+console.log("2"==2); //checks only values by conversion
+console.log("2"===2) //strict check,checks data type as well
