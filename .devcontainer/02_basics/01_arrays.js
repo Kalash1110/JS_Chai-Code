064981b749abb157a791d3c505293f373e08dfd1
Kalash1110 <65525443+Kalash1110@users.noreply.github.com>
2023-12-20 18:17:03 +0000

    arrays

---
+++ .devcontainer/02_basics/01_arrays.js
@@ -0,0 +1,37 @@
+const myArray=[1,2,3,4,5,true,"Kalash"]
+console.log(typeof myArray); //type is object 
+
+const anotherWay=new Array(1,2,3)
+
+anotherWay.push("Jha") //Add values in array
+
+anotherWay.pop() //removes last value
+
+
+
+
+
+anotherWay.unshift(10) //adds value in front but slow as it shifts the entire array
+
+anotherWay.shift() //removes value from first  index
+
+
+console.log(anotherWay.includes(3)); //checks whether that value is present in array or not
+
+console.log(anotherWay.indexOf(3)); //gives index of values in array
+
+const arrayString=anotherWay.join() //binds the array into one string
+// console.log(arrayString);
+// console.log(typeof arrayString); //type is string not object
+
+
+//SLICE AND SPLICE IMPORTANT 
+
+const arr=[1,2,3,4,5,6,7,8]
+console.log("Start array og: ",arr);
+const sl=arr.slice(2,5)
+console.log("sliced array",sl);
+console.log("After slice og array: ",arr);
+const spl=arr.splice(2,5)
+console.log("spliced array",spl);
+console.log("After splice og array",arr);
