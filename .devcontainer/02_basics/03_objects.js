49bd1755ad44753ab858badb912998005f819366
Kalash1110 <65525443+Kalash1110@users.noreply.github.com>
2023-12-21 11:17:50 +0000

    Objects

--- .devcontainer/02_basics/03_objects.js
+++ .devcontainer/02_basics/03_objects.js
@@ -19,3 +19,23 @@ console.log(user["full name"]);
 
 console.log(user[x]); //syntax to access symbols
 console.log(typeof x); 
+
+
+user.isLogged=true //value change or override
+
+//Even after changing values after freeze it won't be reflected in the object (throws no error)
+// Object.freeze(user) 
+
+user.isLogged=false; //no chnage as object is already freezed
+console.log(user.isLogged); //gives true
+
+//Adding function to the object 
+user.greet=function(){
+    console.log(`Welcome!!,${this.name}`);
+    //using this to get the object's variable
+}
+
+
+
+//Using that function
+console.log(user.greet());
