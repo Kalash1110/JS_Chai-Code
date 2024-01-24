2c6ca844520f85fec95491d207ea28d33c645b75
Kalash1110 <65525443+Kalash1110@users.noreply.github.com>
2024-01-24 10:43:12 +0000

    for_loop

---
+++ .devcontainer/05_iterations/one.js
@@ -0,0 +1,16 @@
+//For loop 
+for (let index = 0; index < 10; index++) {
+    console.log(index);
+    
+    
+}
+
+let myArray=["Kalash","Yashi","Patu","Sristy"]
+
+for(let idx=0;idx<myArray.length;idx++)
+    console.log(myArray[idx]);
+
+    //Note if your write idx<=myArray.length,you won't get error,you will get value printed as undefined as there are no out of bound exceptions in JS
+
+
+//Break and continue are similar to java
